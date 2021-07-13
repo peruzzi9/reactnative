c8a91812af8ba719ab7e645527781973efc9342d
peruzzi9 <juventino30@hotmail.com>
2021-07-13 12:19:47 +0300

    Styling CSS

--- App.js
+++ App.js
@@ -1,24 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text,Button, View } from 'react-native';
+import { StyleSheet, Text,Button, View , Pressable } from 'react-native';
 
 export default function App() {
  let [counter, setcounter] = useState(0) 
   return (
     <View style={styles.container}>
-          <Text>Counter Value is !.... {counter} </Text> 
+          <Text style={styles.text}>Counter Value is !.... {counter} </Text> 
           <Button onPress={()=>setcounter(++counter)} title="Add" color="#000000" accessibilityLabel="Tap on Me"/>
           <Button onPress={()=>setcounter(--counter)} title="Reduce" color="#555555" accessibilityLabel="Tap on Me"/>
-          <Button onPress={()=>alert("Hello to my first app with react native ...")}  title="Hello alert" color="#555777" />
+          <Pressable style={styles.button} 
+                     onPress={()=>alert("Hello to my first app with react native ...")}>
+            <Text style={styles.buttonText}>Hello Alert...</Text>
+          </Pressable>
           <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  body : { backgroundColor:'#fff979'},
+  text : { 
+    color:'#000999',
+    fontSize :20 ,
+    fontStyle:'italic',
+    margin:10,
+    padding:10
+  },
+  buttonText : { 
+    color:'#fff979',
+    fontSize :20 ,
+    fontStyle:'italic',
+    margin:10,
+    padding:10
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 30,
+    elevation: 100,
+    margin : 10,
+    backgroundColor: '#224e79',
+    color:'#d9e447'
+  }, 
   container: {
     flex: 1,
-    backgroundColor: '#fff999',
+    backgroundColor: '#fffff1',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+
+# === 13/07/2021 1 ===
+# Define Styles example
+- Add new style / body / Text / color /font /background 
+- Note : React Native's <Button /> component does not accept a style prop, and its color prop is limited and appears differently across Android, iOS, and the web. With the <Pressable /> component, we can create custom buttons that fit our app's design. Those styles will also be the same across Android, iOS, and the web, which will give our apps a consistent look on every platform.
+
 # === 08/07/2021 2 ===
 # Simple counter with add/reduce useing useState hook
 - add Simple counter with add/reduce useing useState hook
